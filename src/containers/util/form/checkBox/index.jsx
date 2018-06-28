664ac910d4977a9b5538413da1f8bb84d2b9e5e6
xiaolong wang <xiaolong wang>
2018-06-29 01:37:39 +0800

    d

--- src/containers/util/form/checkBox/index.jsx
+++ src/containers/util/form/checkBox/index.jsx
@@ -3,64 +3,16 @@ import ReactDOM,{render} from 'react-dom';
 import style from './index.scss'
 import no from './select-no.png'
 import yes from './select-yes.png'
-import { autobind } from 'core-decorators'
 import classnames from 'classnames'
-import {bindActionCreators} from "redux";
-import {connect} from "react-redux";
 import actions from 'src/actions/userInfo'
-// import { defaultProps } from 'src/decorators'
-const defaultProps = function (defaultProps) {
-    return function (target) {
-        target.defaultProps = defaultProps
-    }
-}
-const loading = function (promise) {
-    return function (target) {
-        let render = target.prototype.render;
-        let componentWillMount = target.prototype.componentWillMount;
-        target.prototype.componentWillMount = function () {
-            this.setState({
-                loading:true
-            })
-            if(promise) promise(this.props,this.state).then(()=>{
-                this.setState({
-                    loading:false
-                })
-            })
-            if(componentWillMount) componentWillMount.apply(this)
-        }
-        target.prototype.render = function () {
-            if(this.state.loading) return(<div>loading</div>)
-            return render.apply(this)
-        }
-        return target
-    }
-}
+import { defaultProps , loading, login, connect, setTitle, autobind} from 'src/decorators'
 
-const login = function () {
-    return function (target) {
-        let render = target.prototype.render;
-        let componentWillMount = target.prototype.componentWillMount;
-        target.prototype.render = function () {
-            if(!this.props.userInfo || !this.props.userInfo.isLogin){
-                return (<div>no login</div>)
-            }
-            return render.apply(this)
-        }
-        target.prototype.componentWillMount = function () {
 
-            if(componentWillMount) componentWillMount.apply(this)
-        }
-    }
-}
 
-@defaultProps({select:'dsf'})
-@connect(state=>{
-    let { userInfo } = state
-    return { userInfo }
-},dispatch=>{
-    return bindActionCreators(actions,dispatch)
-})
+console.log(store)
+@setTitle('test')
+@defaultProps({select:false})
+@connect(['userInfo'],actions)
 @loading(async (props,state)=>{
     let userinfo = props.getUserInfo()
     await Promise.all([userinfo]);
